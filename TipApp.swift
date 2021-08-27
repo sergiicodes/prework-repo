b13d66681d02ac20f948a57966b8ed02e28f593d
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2021-08-27 13:57:07 -0500

    Add files via upload

---
+++ TipApp.swift
@@ -0,0 +1,42 @@
+//used VS, couldnt use Xcode
+//import 
+import UIKit
+
+//each field 
+class ViewController: UIViewController {
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+//rescind keyboard
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+//calcualtion
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //tip amnt
+        let tipPercent = [0.15,0.18,0.2]
+
+        let bill = Double(BillField.text!) ?? 0
+
+        let tip = bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+
+        //decimal to 2 places
+        tipLabel.text = String(format: "$%/.2f",tip)
+        totalLabel.text = String(format: "$%.2f",total)
+    }
+
+}
